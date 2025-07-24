d73013bcbecb7a76167c7aef9c29f07e21830a7d
Bjoern Mayer <bjoern.mayer92@proton.me>
2025-07-24 21:37:06 +0200

    Refactor database.py

--- database.py
+++ database.py
@@ -1,14 +1,18 @@
 import sqlite3
 import sys, os
 
-
-def create_database(path):
+def connect_to_database(path):
 	try:
 		with sqlite3.connect(path) as conn:
 			print(f"Opened Sqlite database version {sqlite3.sqlite_version}")
 	except sqlite3.Error as e:
-		print(f"An Error occurred {e}")
+		print(f"An Error occcurred {e}")
+	return conn
 
+def create_database(path):
+	conn = connect_to_database(path)
+
+	
 	with open("setup_db.sql", "r") as f:
 		setup_script = f.read()
 
